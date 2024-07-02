c73eab145476706fee29ea0f23060238619d8202
Chapman Shoop <chapman.shoop@gmail.com>
2024-07-02 07:17:24 -0500

    Initial Commit

---
+++ Select Random Files Mac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Select Random Files Mac
+//
+//  Created by Chapman on 7/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Select Random Files Mac/Select_Random_Files_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Select_Random_Files_MacApp.swift
+//  Select Random Files Mac
+//
+//  Created by Chapman on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Select_Random_Files_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
