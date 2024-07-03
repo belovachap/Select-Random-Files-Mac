657600e4e55a95d51d052f610342237296289a54
Chapman Shoop <chapman.shoop@gmail.com>
2024-07-02 21:00:05 -0500

    Renaming to 'Copy Random Files'

--- Copy Random Files/Copy_Random_FilesApp.swift
+++ Copy Random Files/Copy_Random_FilesApp.swift
@@ -1,6 +1,6 @@
 //
-//  Select_Random_Files_MacApp.swift
-//  Select Random Files Mac
+//  Copy_Random_FilesApp.swift
+//  Copy Random Files
 //
 //  Created by Chapman on 7/2/24.
 //
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct Select_Random_Files_MacApp: App {
+struct Copy_Random_FilesApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
